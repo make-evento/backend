747c059c9c79d5c83d4641b7993bee6aa2507d61
Bruno Cerciliar <brunocerciliar@Brunos-MacBook-Air.local>
2024-07-22 17:59:15 -0300

    first commit

---
+++ app/Models/ProposalCustomer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProposalCustomer extends Model
+{
+    use HasFactory;
+}
